1df896d7fa4885c75b5570883d1bc135d4a768ef
Tijn Kersjes <tijn@divbyzero.nl>
2025-02-16 15:49:07 +0100

    Add identity and const to fun.lib

--- fun.lua
+++ fun.lua
@@ -1,5 +1,15 @@
 function noop() end
 
+function identity(x)
+    return x
+end
+
+function const(x)
+    return function()
+        return x
+    end
+end
+
 function bind(fn, me)
     return function(...)
         return fn(me, ...)
