13e5559529f404389ecc7dde65e8fc17af736cc4
Tijn Kersjes <tijn@divbyzero.nl>
2025-12-28 18:24:33 +0100

    Add LuaJIT variant of RNG32 module

---
+++ rng32jit.lua
@@ -0,0 +1,44 @@
+local bit = require("bit")
+
+local function xorshift(x)
+    -- x = (x ~ x << 13) & 2147483647
+    -- x = (x ~ x >> 17) & 2147483647
+    -- x = (x ~ x << 5) & 2147483647
+    x = bit.band(bit.bxor(x, bit.lshift(x, 13)), 2147483647)
+    x = bit.band(bit.bxor(x, bit.rshift(x, 17)), 2147483647)
+    x = bit.band(bit.bxor(x, bit.lshift(x, 5)), 2147483647)
+    return x
+end
+
+local RNG = {}
+RNG.__index = RNG
+
+function RNG.new(seed)
+    local self = setmetatable({}, RNG)
+    self:seed(seed or 1)
+    return self
+end
+
+function RNG:seed(seed)
+    -- self.state = (seed * 23456789) & 2147483647
+    self.state = bit.band(seed * 23456789, 2147483647)
+end
+
+function RNG:next()
+    local x = xorshift(self.state)
+    self.state = x
+    return x
+end
+
+function RNG:random(a, b)
+    local x = self:next()
+    if not a then
+        return x / 2147483647
+    elseif not b then
+        return x % a + 1
+    else
+        return x % (b - a + 1) + a
+    end
+end
+
+return RNG
